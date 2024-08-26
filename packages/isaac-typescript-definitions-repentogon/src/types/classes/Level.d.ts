8b94aef7a9cd3bf4e46933291deebdb0434aac99
Syntax Sculptor <143585501+Syntax-Sculptor@users.noreply.github.com>
2024-08-26 08:20:33 -0700

    Update Level.d.ts

--- packages/isaac-typescript-definitions-repentogon/src/types/classes/Level.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/Level.d.ts
@@ -2,14 +2,64 @@ import type {
   Dimension,
   DoorSlot,
   LevelStage,
+  RoomShape,
   StageType,
 } from "isaac-typescript-definitions";
 import type { SpecialQuest } from "../../enums/SpecialQuest";
 
 declare global {
   interface Level extends IsaacAPIClass {
+    /**
+     * Returns whether the room can fit in the provided `gridIndex`.
+     *
+     * @param dimension Optional. Default is `Dimension.CURRENT`.
+     * @param allowMultipleDoors Optional. Determines if the room should be placed if it has multiple doors. Default is true.
+     * @param allowSpecialNeighbors Optional. Determines if the room can have connections to special rooms. Secret rooms are always allowed, but boss rooms are never allowed. Default is false.
+     * @param allowNoNeighbors Optional. Determines if the room can be placed without having any neighbors. Default is false.
+     */
+    CanPlaceRoom: (
+      roomConfig: RoomConfig,
+      gridIndex: int,
+      dimension?: Dimension,
+      allowMultipleDoors?: boolean,
+      allowSpecialNeighbors?: boolean,
+      allowNoNeighbors?: boolean,
+    ) => boolean;
+
+    /**
+     * Returns whether the room can be placed at the provided `doorSlot`.
+     *
+     * @param roomConfig
+     * @param roomDescriptor
+     * @param doorSlot
+     * @param allowMultipleDoors Optional. Determines if the room should be placed if it has multiple doors. Default is true.
+     * @param allowSpecialNeighbors Optional. Determines if the room can have connections to special rooms. Secret rooms are always allowed, but boss rooms are never allowed. Default is false.
+     */
+    CanPlaceRoomAtDoor: (
+      roomConfig: RoomConfig,
+      roomDescriptor: RoomDescriptor,
+      doorSlot: DoorSlot,
+      allowMultipleDoors?: boolean,
+      allowSpecialNeighbors?: boolean,
+    ) => boolean;
+
     /** Returns whether the Red Door outline can spawn at the specified `DoorSlot`. */
     CanSpawnDoorOutline: (roomIndex: int, doorSlot: DoorSlot) => boolean;
+
+    /**
+     * Returns an array of grid indexes which are valid locations to place the given room.
+     *
+     * @param roomConfig
+     * @param dimension Optional. Default is `Dimension.CURRENT`.
+     * @param allowMultipleDoors Optional. Determines if the room should be placed if it has multiple doors. Default is true.
+     * @param allowSpecialNeighbors Optional. Determines if the room can have connections to special rooms. Secret rooms are always allowed, but boss rooms are never allowed. Default is false.
+     */
+    FindValidRoomPlacementLocations: (
+      roomConfig: RoomConfig,
+      dimension?: Dimension,
+      allowMultipleDoors?: boolean,
+      allowSpecialNeighbors?: boolean,
+    ) => ReadonlyArray<int>;
 
     /** Returns the current dimension the player is in. */
     GetDimension: () => Dimension;
@@ -19,6 +69,21 @@ declare global {
      * effect.
      */
     GetMyosotisPickups: () => EntitiesSaveStateVector;
+
+    /**
+     * Returns a dictionary containing neighboring rooms from the provided `gridIndex`. This can be used to determine the neighbors of a room before placing it.
+     *
+     * This method does not signal if a room would actually fit or if the neighbors would allow a connection.
+     *
+     * @param gridIndex
+     * @param roomShape
+     * @param dimension Optional. Default is `Dimension.CURRENT`.
+     */
+    GetNeighboringRooms: (
+      gridIndex: int,
+      roomShape: RoomShape,
+      dimension?: Dimension,
+    ) => LuaTable<DoorSlot, RoomDescriptor>;
 
     /** Returns whether the special quest is forced. */
     GetForceSpecialQuest: () => SpecialQuest;
@@ -59,5 +124,45 @@ declare global {
 
     /** Sets the display name of the level. */
     SetName: (name: string) => void;
+
+    /**
+     * Attempts to place a room on the grid. If the room is placed successfully, its `RoomDescriptor` is returned. Returns undefined if the room was not placed successfully.
+     *
+     * @param roomConfig
+     * @param gridIndex
+     * @param dimension Optional. Default is `Dimension.CURRENT`.
+     * @param seed Optional. If undefined or set to 0, the seed will be set based on the location, room shape, and level seed. Default is undefined.
+     * @param allowMultipleDoors Optional. Determines if the room should be placed if it has multiple doors. Default is true.
+     * @param allowSpecialNeighbors Optional. Determines if the room can have connections to special rooms. Secret rooms are always allowed, but boss rooms are never allowed. Default is false.
+     * @param allowNoNeighbors Optional. Determines if the room can be placed without having any neighbors. Default is false.
+     */
+    TryPlaceRoom: (
+      roomConfig: RoomConfig,
+      gridIndex: int,
+      dimension?: Dimension,
+      seed?: Seed,
+      allowMultipleDoors?: boolean,
+      allowSpecialNeighbors?: boolean,
+      allowNoNeighbors?: boolean,
+    ) => RoomDescriptor | undefined;
+
+    /**
+     * Tries to place a room at the provided `doorSlot`.
+     *
+     * @param roomConfig
+     * @param roomDescriptor
+     * @param doorSlot
+     * @param seed Optional. If undefined or set to 0, the seed will be set based on the location, room shape, and level seed. Default is undefined.
+     * @param allowMultipleDoors Optional. Determines if the room should be placed if it has multiple doors. Default is true.
+     * @param allowSpecialNeighbors Optional. Determines if the room can have connections to special rooms. Secret rooms are always allowed, but boss rooms are never allowed. Default is false.
+     */
+    TryPlaceRoomAtDoor: (
+      roomConfig: RoomConfig,
+      roomDescriptor: RoomDescriptor,
+      doorSlot: DoorSlot,
+      seed?: Seed,
+      allowMultipleDoors?: boolean,
+      allowSpecialNeighbors?: boolean,
+    ) => boolean;
   }
 }
