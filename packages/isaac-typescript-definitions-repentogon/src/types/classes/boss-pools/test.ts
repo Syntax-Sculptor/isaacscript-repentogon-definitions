751fa72856672d0ea3ec8059bc5e773e07a5d2ef
syntax-sculptor <syntaxsculptor@proton.me>
2024-09-06 07:04:53 -0800

    fix: forgot to remove quick test file

--- packages/isaac-typescript-definitions-repentogon/src/types/classes/boss-pools/test.ts
+++
@@ -1,9 +0,0 @@
-import { BossID } from "isaac-typescript-definitions";
-
-const removedBosses = BossPoolManager.GetRemovedBosses();
-
-if (removedBosses.has(BossID.RAINMAKER)) {
-  // Monstro hasn't been removed from the boss pool.
-} else {
-  // Monstro has been removed from the boss pool.
-}
