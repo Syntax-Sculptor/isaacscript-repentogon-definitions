0f8dc675dcd77674c5b489069cb10349c78e8d70
SyntaxSculptor <SyntaxSculptor@proton.me>
2024-09-06 11:24:34 -0700

    docs: debug changes

--- packages/isaac-typescript-definitions-repentogon/src/types/classes/Debug.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/Debug.d.ts
@@ -5,16 +5,17 @@
  * @noSelf
  */
 declare namespace Debug {
-  /** Forcibly unloads a specified script from the Lua environment. */
+  /** Forcibly unloads the specified file from the Lua environment. */
   function ForceUnload(moduleName: string): void;
 
   /**
-   * Retrieves the function signature from the provided address.
+   * Retrieves the function signature from the provided memory address.
    *
-   * Passing an invalid address will throw an error.
+   * Passing an invalid address will result in an error. It's recommended to handle this method in a
+   * pcall as if no signature was found or the memory address was invalid, an error will be thrown.
    */
   function GetSignature(address: int): string;
 
-  /** Returns an array of all scripts currently loaded in the Lua environment. */
+  /** Returns an array of all file names currently loaded in the Lua environment. */
   function ListLoadedFiles(): string[];
 }
