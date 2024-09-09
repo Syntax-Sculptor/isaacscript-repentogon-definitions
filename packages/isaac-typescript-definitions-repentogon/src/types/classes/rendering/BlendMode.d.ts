ca1c9f05af4c97109795e289fd59f826ebe4f282
SyntaxSculptor <SyntaxSculptor@proton.me>
2024-09-09 08:05:34 -0700

    docs: BlendMode

--- packages/isaac-typescript-definitions-repentogon/src/types/classes/rendering/BlendMode.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/rendering/BlendMode.d.ts
@@ -3,6 +3,15 @@ import type { BlendType } from "../../../enums/BlendType";
 
 declare global {
   /**
+   * This class determines how a `LayerState` should blend with the elements rendered behind it. You
+   * can retrieve this class by calling the `LayerState.GetBlendMode` method:
+   *
+   * ```ts
+   * const sprite = Sprite();
+   * const layer = sprite.GetLayer(0);
+   * const blendMode = layer.GetBlendMode();
+   * ```
+   *
    * This class is for REPENTOGON, an exe-hack which expands the modding API.
    *
    * @see https://repentogon.com/
