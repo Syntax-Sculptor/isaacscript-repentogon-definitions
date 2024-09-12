776bd4231d120e8d514eb205ee140b07100f0bf5
SyntaxSculptor <syntaxsculptor@proton.me>
2024-09-12 14:32:19 -0700

    docs: GridEntity

--- packages/isaac-typescript-definitions-repentogon/src/types/classes/grid-entity/GridEntityDoor.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/grid-entity/GridEntityDoor.d.ts
@@ -2,7 +2,10 @@ import type { DoorState, DoorVariant } from "isaac-typescript-definitions";
 
 declare global {
   interface GridEntityDoor extends GridEntity {
+    /** Returns the door's extra sprite. */
     GetExtraSprite: () => Sprite;
+
+    PlayAnimation: () => void;
 
     PreviousState: DoorState;
     PreviousVariant: DoorVariant;
